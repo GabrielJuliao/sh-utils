3700c8cf421981c138e1fe52094f1ecf4f22007c
gabrieljuliao <gabrielbjuliao@gmail.com>
2024-06-14 15:37:28 -0300

    Full path backup & validate url

--- git/swap_remote_origin_url_with_expression.sh
+++ git/swap_remote_origin_url_with_expression.sh
@@ -7,34 +7,34 @@
 
 set -e
 
-# Function to backup the current remote URL
+# Function to backup the current remote URL and repository path
 # Arguments:
 # $1 - Current remote URL
-# $2 - Repository name (directory name)
+# $2 - Repository directory path
 backup_remote_url() {
   local remote_url=$1
-  local repo_name=$2
-  local backup_dir="$HOME/git_migration_backup/$repo_name"
+  local repo_path=$2
+  local backup_dir="$HOME/git_migration_backup"
 
   # Create backup directory if it doesn't exist
   mkdir -p "$backup_dir"
 
   # Save the remote URL backup to a file in the backup directory
-  echo "$remote_url" > "$backup_dir/git_remote_url.backup"
+  echo "$remote_url" > "$backup_dir/${repo_path//\//_}_git_remote_url.backup"
 }
 
 # Function to restore the original remote URL from backup
 # Arguments:
-# $1 - Repository name (directory name)
+# $1 - Repository directory path
 restore_remote_url() {
-  local repo_name=$1
-  local backup_dir="$HOME/git_migration_backup/$repo_name"
-  local backup_file="$backup_dir/git_remote_url.backup"
+  local repo_path=$1
+  local backup_dir="$HOME/git_migration_backup"
+  local backup_file="$backup_dir/${repo_path//\//_}_git_remote_url.backup"
 
   if [ -f "$backup_file" ]; then
     local original_url=$(cat "$backup_file")
     echo "Restoring original remote URL: $original_url"
-    if git -C "$repo_name" remote set-url origin "$original_url"; then
+    if git -C "$repo_path" remote set-url origin "$original_url"; then
       echo "Successfully restored original remote URL"
       rm "$backup_file" # Remove backup file after successful restore
     else
@@ -43,6 +43,15 @@ restore_remote_url() {
   else
     echo "Backup file '$backup_file' not found, unable to restore original remote URL"
   fi
+}
+
+# Function to validate URL format
+# Arguments:
+# $1 - URL to validate
+validate_url() {
+  local url=$1
+  local regex='^(https|ssh):\/\/[a-zA-Z0-9.-]+(:[0-9]+)?\/?.*$'
+  [[ "$url" =~ $regex ]]
 }
 
 # Function to process repository based on parameters
@@ -66,10 +75,10 @@ process_repository() {
     return
   fi
 
-  # Backup the current remote URL
-  local repo_name=$(basename "$dir")
-  backup_remote_url "$remote_url" "$repo_name"
-  echo "Backed up the original remote URL to $HOME/git_migration_backup/$repo_name in directory: $dir"
+  # Backup the current remote URL with full repository path
+  local repo_path=$(pwd)
+  backup_remote_url "$remote_url" "$repo_path"
+  echo "Backed up the original remote URL to $HOME/git_migration_backup/${repo_path//\//_}_git_remote_url.backup in directory: $dir"
 
   # Convert URL using sed expression
   local new_url
@@ -77,12 +86,17 @@ process_repository() {
   echo "Old URL: $remote_url"
   echo "New URL: $new_url"
 
-  # Set the new remote URL for 'origin'
-  echo "Setting new remote URL for 'origin'"
-  if git -C "$dir" remote set-url origin "$new_url"; then
-    echo "Successfully set new remote URL for 'origin' in directory: $dir"
+  # Validate the new URL
+  if validate_url "$new_url"; then
+    # Set the new remote URL for 'origin'
+    echo "Setting new remote URL for 'origin'"
+    if git remote set-url origin "$new_url"; then
+      echo "Successfully set new remote URL for 'origin' in directory: $dir"
+    else
+      echo "Failed to set new remote URL for 'origin' in directory: $dir"
+    fi
   else
-    echo "Failed to set new remote URL for 'origin' in directory: $dir"
+    echo "Invalid new URL: $new_url"
   fi
 
   cd .. # Return to the parent directory
@@ -106,9 +120,10 @@ process_repositories() {
   # Check if --restore parameter is provided
   if [ "$action" == "--restore" ]; then
     # Iterate through all backup directories and restore
-    for backup_dir in "$HOME/git_migration_backup"/*/; do
-      local repo_name=$(basename "$backup_dir")
-      restore_remote_url "$repo_name"
+    for backup_file in "$HOME/git_migration_backup"/*_git_remote_url.backup; do
+      local repo_path=${backup_file%_git_remote_url.backup}
+      repo_path=${repo_path##*/}
+      restore_remote_url "$repo_path"
     done
     return
   fi
@@ -130,7 +145,7 @@ main() {
 
   # Validate action parameter
   if [ "$action" != "--replace" ] && [ "$action" != "--restore" ]; then
-    echo "Invalid action parameter. Usage: ./my-script.sh [--replace <sed_expression>] [--restore] [<directory>]"
+    echo "Invalid action parameter. Usage: $0 [--replace <sed_expression>] [--restore] [<directory>]"
     exit 1
   fi
 
